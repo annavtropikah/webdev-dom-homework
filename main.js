4d9c9fa1d6d5e0b34b7b3a4dd7e2c982df9357bb
«Annavtropikah» <kozhevnikova.it@gmail.com>
2023-12-20 18:56:01 +0300

    ДЗ 15 без лайков

--- main.js
+++ main.js
@@ -3,33 +3,23 @@
 import { getComments, postComment } from './api.js';
 import { sanitizeHtml } from './sanitizeHtml.js';
 import { formatDateTime } from './formatDateTime.js';
-
-
+import { renderComment } from './render.js';
 
 const buttonElement = document.getElementById('add-button');
-const listElement = document.getElementById("list");
 const nameInputElement = document.getElementById("name-input");
 const textareaInputElement = document.getElementById("textarea-input");
 const commentsLoading = document.getElementById("comments-loading");
 const addForm = document.getElementById("add-form");
 const addComment = document.getElementById("add-comment");
 
-// const formatDateTime = (date) => {
-//     const day = String(date.getDate()).padStart(2, '0');
-//     const month = String(date.getMonth() + 1).padStart(2, '0');
-//     const year = String(date.getFullYear() - 2000);
-//     const minutes = String(date.getMinutes()).padStart(2, '0');
-//     const hours = String(date.getHours()).padStart(2, '0');
-//     return `${day}.${month}.${year} ${hours}:${minutes}`;
-// };
+
+let comments = [];
 
 const fetchAndRenderComments = () => {
     getComments().then((responceData) => {
         const appComments = responceData.comments.map((comment) => {
             return {
-                //достаем имя автора
                 name: comment.author.name,
-                //преобразовываем дату в Date
                 date: formatDateTime(new Date(comment.date)),
                 text: comment.text,
                 likes: comment.likes,
@@ -38,39 +28,13 @@ const fetchAndRenderComments = () => {
         });
         commentsLoading.classList.add("hidden");
         comments = appComments;
-        renderComment();
+        renderComment({ comments });
+        
     });
 };
 
 fetchAndRenderComments();
 
-//рендер комментария
-let comments = [];
-const renderComment = () => {
-    const commentHtml = comments.map((comment, index) => {
-        return `<li class="comment" data-name="${comment.name}" data-text="${comment.text}">
-          <div class="comment-header">
-            <div>${comment.name}</div>
-            <div>${comment.date}</div>
-          </div>
-          <div class="comment-body">
-            <div class="comment-text">
-              ${comment.text.replaceAll("BEGIN_QUOTE", "<div class='quote'>").replaceAll("END_QUOTE", "</div>")}
-            </div>
-          </div>
-          <div class="comment-footer">
-            <div class="likes">
-              <span class="likes-counter">${comment.likes}</span>
-              <button class="like-button ${comment.isLiked ? "-active-like" : ''}" data-index="${index}"></button>
-            </div>
-          </div>
-          </li>`;
-    }).join('');
-
-    listElement.innerHTML = commentHtml;
-    initLikeListeners();
-    initAnswerCommentListeners();
-};
 
 const initLikeListeners = () => {
     const likeButtons = document.querySelectorAll('.like-button');
@@ -81,7 +45,7 @@ const initLikeListeners = () => {
             comments[index].likes += comments[index].isLiked ? -1 : +1;
             comments[index].isLiked = !comments[index].isLiked;
 
-            renderComment();
+            renderComment({comments});
         });
     }
 };
@@ -99,7 +63,7 @@ const initAnswerCommentListeners = () => {
         });
     }
 }
-//new comment
+// new comment
 
 const addNewComment = () => {
     nameInputElement.classList.remove("error");
@@ -126,12 +90,12 @@ const addNewComment = () => {
         .then((response) => {
             if (response.status === 201) {
                 fetchAndRenderComments()
-                    .then(() => {
-                        nameInputElement.value = "";
-                        textareaInputElement.value = '';
-                        addForm.classList.remove("hidden");
-                        addComment.classList.add("hidden");
-                    })
+
+                nameInputElement.value = "";
+                textareaInputElement.value = '';
+                addForm.classList.remove("hidden");
+                addComment.classList.add("hidden");
+
                 return
             }
             if (response.status === 400) {
@@ -152,8 +116,8 @@ const addNewComment = () => {
         })
 
 
-    renderComment();
+    renderComment({ comments });
 };
 
 buttonElement.addEventListener('click', addNewComment);
-renderComment();
+renderComment({ comments });
